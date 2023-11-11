c80ed9b3e34c6392ebeb7f548e52851716d09749
unknown <trantrungduc100197@gmail.com>
2023-11-11 09:19:24 +0700

    update cmd

--- server.js
+++ server.js
@@ -6,8 +6,7 @@ const TelegramBot = require('node-telegram-bot-api');
 
 const bot = new TelegramBot(TOKEN, { polling: true });
 
-// Handle '/echo' command
-bot.onText(/\/echo (.+)/, (msg, match) => {
+bot.onText(/\/start/, (msg, match) => {
   const chatId = msg.chat.id;
   const resp = match[1];
 
@@ -16,24 +15,27 @@ bot.onText(/\/echo (.+)/, (msg, match) => {
 
 bot.on('message', async (msg) => {
   const chatId = msg.chat.id;
-  const exchangeRates = await fetchExchangeRates();
-  const option = {
-    "parse_mode": "HTML",
-  };
 
-  const sentMessageRes = `${bold('Tỷ giá USD - Digital Banks')}\n\n${Object.keys(exchangeRates).map((bankName) => {
-    const buyPrice = exchangeRates[bankName].buy;
-    const sellPrice = exchangeRates[bankName].sell;
-    return `${bankName}\n  MUA: ${bold(buyPrice)} VND\n  BÁN: ${bold(sellPrice)} VND`;
-  }).join("\n\n")
-    }`;
-  bot.sendMessage(chatId, sentMessageRes,option)
-    .then((sentMessage) => {
-      setTimeout(() => {
-        bot.deleteMessage(chatId, sentMessage.message_id);
-        sendMessageAndDeleteAgain(chatId);
-      }, TIME); //one hour
-    });
+  if (msg.text == '/start') {
+    const exchangeRates = await fetchExchangeRates();
+    const option = {
+      "parse_mode": "HTML",
+    };
+
+    const sentMessageRes = `${bold('Tỷ giá USD - Digital Banks')}\n\n${Object.keys(exchangeRates).map((bankName) => {
+      const buyPrice = exchangeRates[bankName].buy;
+      const sellPrice = exchangeRates[bankName].sell;
+      return `${bankName}\n  MUA: ${bold(buyPrice)} VND\n  BÁN: ${bold(sellPrice)} VND`;
+    }).join("\n\n")
+      }`;
+    bot.sendMessage(chatId, sentMessageRes, option)
+      .then((sentMessage) => {
+        setTimeout(() => {
+          bot.deleteMessage(chatId, sentMessage.message_id);
+          sendMessageAndDeleteAgain(chatId);
+        }, TIME); //one hour
+      });
+  }
 });
 
 // Recurring gold price updates
@@ -49,13 +51,15 @@ async function sendMessageAndDeleteAgain(chatId) {
   }).join("\n\n")
     }`;
 
-  bot.sendMessage(chatId, sentMessageRes,option)
-    .then((sentMessage) => {
-      setTimeout(() => {
-        bot.deleteMessage(chatId, sentMessage.message_id);
-        sendMessageAndDeleteAgain(chatId);
-      }, TIME); //one hour
-    });
+  if (msg.text == '/start') {
+    bot.sendMessage(chatId, sentMessageRes, option)
+      .then((sentMessage) => {
+        setTimeout(() => {
+          bot.deleteMessage(chatId, sentMessage.message_id);
+          sendMessageAndDeleteAgain(chatId);
+        }, TIME); //one hour
+      });
+  }
 }
 
 function bold(text) {
